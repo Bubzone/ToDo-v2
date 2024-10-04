2aaf1b93107114b6777c39eb424c8ae87fb5df9b
Bubzone <kubaszmajdzinski@gmail.com>
2024-10-04 11:11:50 +0200

    adding just layout of web, app.py dont have anything special for now

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, flash, redirect, render_template, request, session
+#from flask_session import Session
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
